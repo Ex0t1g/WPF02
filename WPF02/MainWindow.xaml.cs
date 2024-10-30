d6ce147d2bca5c9528b897634948231b0171469b
Administrator <Administrator@DESKTOP-KGTT1NE>
2024-10-30 15:02:31 +0300

    Добавьте файлы проекта.

---
+++ WPF02/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WPF02;
+
+
+public partial class MainWindow : Window
+{
+    private static readonly IReadOnlyList<Brush> brushes = new[] {
+        Brushes.Red, Brushes.Blue, Brushes.Green, Brushes.Yellow,
+    };
+
+    private static readonly IReadOnlyList<Months> months = new Months[] {
+        new Months ("Январь", 1000),
+        new Months ("Февраль", 2000),
+        new Months ("Март", 3000),
+    };
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        RebuildGrid();
+    }
+
+    private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        RebuildGrid();
+    }
+
+    private void RebuildGrid()
+    {
+        grid.ColumnDefinitions.Clear();
+        grid.Children.Clear();
+
+        int maxValue = months.Select(month => month.Value).Max();
+        for (int i = 0; i < months.Count; i++)
+        {
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
+
+            Months month = months[i];
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Fill = brushes[i % brushes.Count];
+            rectangle.Width = 40;
+            rectangle.Height = month.Value * topRow.ActualHeight / maxValue;
+            rectangle.VerticalAlignment = VerticalAlignment.Bottom;
+            Grid.SetRow(rectangle, 0);
+            Grid.SetColumn(rectangle, i);
+            grid.Children.Add(rectangle);
+
+            TextBlock text = new TextBlock();
+            text.Text = month.Name;
+            text.Margin = new Thickness(5, 0, 5, 0);
+            Grid.SetRow(text, 1);
+            Grid.SetColumn(text, i);
+            grid.Children.Add(text);
+        }
+    }
+}
---
+++ WPF02/Months.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF02
+{
+    public class Months
+    {
+        public string Name { get; }
+        public int Value { get; }
+
+        public Months(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
